4dcc6a0701ebbb9b0d7739935635f2426da0472d
kmiya <miyak19@mytru.ca>
2026-02-28 16:02:34 -0800

    main.py has keyboard Interrupt finish

--- src/capstone_thermal_sensor/main.py
+++ src/capstone_thermal_sensor/main.py
@@ -1,10 +1,10 @@
-
-
+import logging
 from capstone_thermal_sensor.thermal_camera_pub import ThermalPublisher
 
-
-
-if __name__ =="__main__":
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     tp = ThermalPublisher()
-
-    tp.run()
+    try:
+        tp.run()
+    except KeyboardInterrupt:
+        pass
