b4b1c8eef4920078d016a4ef7298ced072cc2435
keishi@capstone <miyak19@mytru.ca>
2026-04-04 21:25:36 -0700

    Adjust human temperature threshold from 34.0℃ to 32.0℃

--- src/capstone_thermal_sensor/Img_predictor.py
+++ src/capstone_thermal_sensor/Img_predictor.py
@@ -87,7 +87,7 @@ class Img_predictor:
                 logger.warning("Invalid temperature data dectected (NaN or Inf)")
                 return None, None
             
-            Human_temp_threshold = 34.0
+            Human_temp_threshold = 32.0
             Target_BG_temp = 20.0
             
             bg_mask = data_array < Human_temp_threshold
